33018cbb75e441d27363245a48fd4e0294ba4476
Enfeathered <gabriel.a.svensk@gmail.com>
2015-01-13 19:21:07 +0100

    Added dependencies

---
+++ SaharaChat/SaharaChat/Hubs/GameHub.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SaharaChat.Hubs
+{
+    public class GameHub : Hub
+    {
+        public override System.Threading.Tasks.Task OnDisconnected(bool stopCalled)
+        {
+
+             return base.OnDisconnected(stopCalled);
+        }
+
+        public void SendMessage(string message)
+        {
+            var connectionId = Context.ConnectionId;
+            Clients.Others.displayMessage(connectionId, message);
+        }
+
+        public void SendPosition(int x, int y)
+        {
+
+        }
+    }
+}
