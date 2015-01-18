3f5f21affae4674b11729436834ae7a4cf3eefdd
Enfeathered <gabriel.a.svensk@gmail.com>
2015-01-18 21:29:41 +0100

    Basic hub/client implemented

--- SaharaChat/SaharaChat/Hubs/GameHub.cs
+++ SaharaChat/SaharaChat/Hubs/GameHub.cs
@@ -9,6 +9,12 @@ namespace SaharaChat.Hubs
 {
     public class GameHub : Hub
     {
+        public override System.Threading.Tasks.Task OnConnected()
+        {
+            Trace.WriteLine("Client connected!!!");
+            return base.OnConnected();
+        }
+
         public override System.Threading.Tasks.Task OnDisconnected(bool stopCalled)
         {
             Trace.Write("Client disconnected!");
@@ -23,12 +29,14 @@ namespace SaharaChat.Hubs
 
         public void SendPosition(int x, int y)
         {
-
+            Trace.WriteLine(string.Format("INCOMING POSITION FROM: {0}\nPOSITION: {1}", Context.ConnectionId, Tuple.Create(x, y)));
+            // Invoke callback in all other clients, informing them of callers GUID and new position
+            Clients.Others.updatePositionOf(Context.ConnectionId, x, y);
         }
 
         public void SayHello()
         {
-            Trace.Write("HELLO!!!  ");
+            Trace.WriteLine("HELLO!!! CONNECTION:  " + Context.ConnectionId);
         }
     }
 }
