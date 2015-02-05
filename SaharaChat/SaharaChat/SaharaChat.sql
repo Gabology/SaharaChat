4168b83053fa5d051786b262b2ffeadbf12a5d01
Enfeathered <gabriel.a.svensk@gmail.com>
2015-02-05 13:47:50 +0100

    Added SQL batch file

---
+++ SaharaChat/SaharaChat/SaharaChat.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE dbo.Users (
+    [Id] int NOT NULL identity(0,1) primary key,
+    [UserName] varchar(50) NOT NULL,
+	[Password] varbinary(255) NOT NULL,
+	[SessionID] varchar(120) NULL
+);
+GO
+
+-- Password hashing trigger
+-- This trigger will automatically hash any password after insertion
+CREATE TRIGGER HashPw
+    ON dbo.Users
+    FOR INSERT
+    AS
+	select HASHBYTES('SHA1', Password) from inserted
+
+insert into Users(UserName, Password) values ('test', 123)
