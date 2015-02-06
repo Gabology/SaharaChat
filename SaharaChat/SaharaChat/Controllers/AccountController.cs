cc9370ec772c8136bb31fa8323b725a05791db06
unclebobsweden <erik.moren@gmail.com>
2015-02-06 12:07:07 +0100

    In med jquery

--- SaharaChat/SaharaChat/Controllers/AccountController.cs
+++ SaharaChat/SaharaChat/Controllers/AccountController.cs
@@ -3,13 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SaharaChat.Models;
 
 namespace SaharaChat.Controllers
 {
     public class AccountController : Controller
     {
         // GET: Account
-        public ActionResult Index()
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(AccountLoginViewModel viewmodel)
+        {
+            //Check if we have both required fields (username and password)
+            if (!ModelState.IsValid)
+            {
+                return View(viewmodel);
+            }
+            
+            //Try to login using our given username and password
+
+
+            return View();
+        }
+
+
+        public ActionResult Logout()
+        {
+            return View();
+        }
+
+        public ActionResult Signup()
         {
             return View();
         }
