550bc902c05389bf0d500ddb60d0ad0883763f33
Enfeathered <gabriel.a.svensk@gmail.com>
2015-02-13 22:53:41 +0100

    Cleanups, implemented UserIdProvider

---
+++ SaharaChat/SaharaChat/Hubs/UserIdProvider.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.SignalR;
+using SaharaChat.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SaharaChat.Hubs
+{
+    public class UserIdProvider : IUserIdProvider
+    {
+        private readonly SaharaContext _db = new SaharaContext();
+
+        public string GetUserId(IRequest request)
+        {
+            return 
+                _db.Users
+                .SingleOrDefault(user => user.SessionID == request.Cookies["ASP.NET_SessionID"].Value)
+                .UserName;
+        }
+    }
+}
--- SaharaChat/SaharaChat/Startup.cs
+++ SaharaChat/SaharaChat/Startup.cs
@@ -1,5 +1,7 @@
 ﻿using Owin;
 using Microsoft.Owin;
+using Microsoft.AspNet.SignalR;
+using SaharaChat.Hubs;
 [assembly: OwinStartup(typeof(SaharaChat.Startup))]
 namespace SaharaChat
 {
@@ -7,6 +9,7 @@ namespace SaharaChat
     {
         public void Configuration(IAppBuilder app)
         {
+            GlobalHost.DependencyResolver.Register(typeof(IUserIdProvider), () => new UserIdProvider());
             // Any connection or hub wire up and configuration should go here
             app.MapSignalR();
         }
